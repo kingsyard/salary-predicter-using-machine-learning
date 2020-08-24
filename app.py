844021b90d5cb4caca6f2a108006befcc8fb24fa
kingsyard <66251076+kingsyard@users.noreply.github.com>
2020-08-24 05:41:03 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,24 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
+import pyrebase
+
+config = {
+	"apiKey": "AIzaSyAD6kvrK16x3nnQVQpnybP90YBSjz0X8Og",
+  "authDomain": "agriculture-81f5b.firebaseapp.com",
+  "databaseURL": "https://agriculture-81f5b.firebaseio.com",
+  "projectId": "agriculture-81f5b",
+  "storageBucket": "agriculture-81f5b.appspot.com",
+  "messagingSenderId": "939998839868",
+  "appId": "1:939998839868:web:bdba7971bfd933c1faef39",
+  "measurementId": "G-9XLTMEPH2K"
+}
+
+firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
+
+
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
 
@@ -17,6 +35,13 @@ def predict():
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
+    a1=final_features[0]
+    b1=final_features[1]
+    c1=final_features[2]
+    print(a1,b1,c1)
+    db.child("names").push({"score1":a1})
+    db.child("names").push({"score2":b1})
+    db.child("names").push({"score3":c1})
 
     output = round(prediction[0], 2)
 
